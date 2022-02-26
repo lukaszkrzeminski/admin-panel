4d2b3e68a2ba78d46028aced52b5c31855e5d615
Lukasz Krzeminski <krzeminski.lukasz@interia.pl>
2022-02-26 19:42:53 +0100

    rendering initial data

--- src/features/users/UsersPage/Table/index.js
+++ src/features/users/UsersPage/Table/index.js
@@ -1,31 +1,36 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectUsers } from "../../usersSlice";
 import { Button, Table, TableCell, TableHeaderCell, TableRow } from "./styled";
 
-export const UsersTable = () => (
-    <Table>
-        <tbody>
-            <TableRow headerRow>
-                <TableHeaderCell>Id</TableHeaderCell>
-                <TableHeaderCell>Name</TableHeaderCell>
-                <TableHeaderCell>Username</TableHeaderCell>
-                <TableHeaderCell>Email</TableHeaderCell>
-                <TableHeaderCell>City</TableHeaderCell>
-                <TableHeaderCell>Edit</TableHeaderCell>
-                <TableHeaderCell>Delete</TableHeaderCell>
-            </TableRow>
-            <TableRow>
-                <TableCell>tmp Id</TableCell>
-                <TableCell>tmp Name</TableCell>
-                <TableCell>tmp Username</TableCell>
-                <TableCell>tpm Email</TableCell>
-                <TableCell>tmp City</TableCell>
-                <TableCell>
-                    <Button editUser>Edit</Button>
-                </TableCell>
-                <TableCell>
-                    <Button deleteUser>Delete</Button>
-                </TableCell>
-            </TableRow>
-        </tbody>
-    </Table>
-)
+export const UsersTable = () => {
+
+    const userTable = useSelector(selectUsers);
+
+    return (
+        <Table>
+            <tbody>
+                <TableRow headerRow>
+                    <TableHeaderCell>Id</TableHeaderCell>
+                    <TableHeaderCell>Name</TableHeaderCell>
+                    <TableHeaderCell>Username</TableHeaderCell>
+                    <TableHeaderCell>Email</TableHeaderCell>
+                    <TableHeaderCell>City</TableHeaderCell>
+                    <TableHeaderCell>Edit</TableHeaderCell>
+                    <TableHeaderCell>Delete</TableHeaderCell>
+                </TableRow>
+                {userTable.map(user =>
+                    <TableRow key={user.id}>
+                        <TableCell>{user.id}</TableCell>
+                        <TableCell>{user.name}</TableCell>
+                        <TableCell>{user.username}</TableCell>
+                        <TableCell>{user.email}</TableCell>
+                        <TableCell>{user.address.city}</TableCell>
+                        <TableCell><Button editUser>Edit</Button></TableCell>
+                        <TableCell><Button deleteUser>Delete</Button></TableCell>
+                    </TableRow>
+                )}
+            </tbody>
+        </Table>
+    )
+}
