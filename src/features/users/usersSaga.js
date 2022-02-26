2a28770f5a09acc5ea1c442a552d05d5d66cf336
Lukasz Krzeminski <krzeminski.lukasz@interia.pl>
2022-02-26 21:33:48 +0100

    added saga

---
+++ src/features/users/usersSaga.js
@@ -0,0 +1,17 @@
+import { takeEvery, call, put } from "redux-saga/effects";
+import { getInitialUsers } from "./getInitialUsers";
+import { fetchInitialUsers, fetchInitialUsersError, fetchInitialUsersSuccess } from "./usersSlice";
+
+function* fetchInitialUsersHandler() {
+    try {
+        const initialUsers = yield call(getInitialUsers)
+        console.log(initialUsers)
+        yield put(fetchInitialUsersSuccess(initialUsers))
+    } catch (error) {
+        yield put(fetchInitialUsersError());
+    }
+}
+
+export function* watchFetchInitialUsers() {
+    yield takeEvery(fetchInitialUsers.type, fetchInitialUsersHandler);
+}
--- src/store.js
+++ src/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
 import usersReducer from './features/users/usersSlice';
+import { watchFetchInitialUsers } from './features/users/usersSaga';
 
-export default configureStore({
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
     reducer: {
         users: usersReducer,
     },
+    middleware: [sagaMiddleware],
 });
+
+sagaMiddleware.run(watchFetchInitialUsers);
+
+export default store;
