0fe4c969d9eca9624fa1e5d52d34c8d2858f90ee
Lukasz Krzeminski <krzeminski.lukasz@interia.pl>
2022-02-27 21:54:45 +0100

    implemented editing users

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -7,12 +7,13 @@ const usersSlice = createSlice({
         loading: true,
         lastId: 10,
         initialStateSet: false,
+        currentId: "",
     },
     reducers: {
         fetchInitialUsers: (state) => {
-            state.loading = true;    
+            state.loading = true;
         },
-        fetchInitialUsersSuccess: (state, { payload: users}) => {
+        fetchInitialUsersSuccess: (state, { payload: users }) => {
             state.users = users;
             state.loading = false;
             state.initialStateSet = true;
@@ -27,6 +28,23 @@ const usersSlice = createSlice({
             state.users.push(payload);
             state.lastId = state.lastId + 1;
         },
+        saveCurrentId: (state, { payload }) => {
+            state.currentId = payload;
+        },
+        editUserData: (state, { payload }) => {
+            if(payload.name !== ""){
+                state.users[payload.id-1].name = payload.name;
+            }
+            if(payload.username !== ""){
+                state.users[payload.id-1].username = payload.username;
+            }
+            if(payload.email !== ""){
+                state.users[payload.id-1].email = payload.email;
+            }
+            if(payload.city !== ""){
+                state.users[payload.id-1].address.city = payload.city;
+            }
+        },
     },
 });
 
@@ -36,11 +54,14 @@ export const {
     fetchInitialUsersError,
     setUsersList,
     addNewUser,
+    saveCurrentId,
+    editUserData,
 } = usersSlice.actions;
 
 const selectUsersState = state => state.users;
 
 export const selectUsers = state => selectUsersState(state).users;
 export const selectLastId = state => selectUsersState(state).lastId;
+export const selectCurrentId = state => selectUsersState(state).currentId;
 export const selectInitialStateSet = state => selectUsersState(state).initialStateSet;
 export default usersSlice.reducer;
