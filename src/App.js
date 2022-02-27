08049ac0efdd3d90a00d555ef86eee99a58e2bd3
Lukasz Krzeminski <krzeminski.lukasz@interia.pl>
2022-02-27 09:39:12 +0100

    App changed to use routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { HashRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Users from './features/users';
+import FormAdd from './features/users/FormAdd';
 
-function App() {
-  return (
-    <Users />
-  );
-}
-
-export default App;
+export default () => (
+  <HashRouter>
+    <Switch>
+      <Route path="/users-list">
+        <Users />
+      </Route>
+      <Route path="/add-new-user">
+        <FormAdd />
+      </Route>
+      <Route path="/">
+        <Redirect to="/users-list"></Redirect>
+      </Route>
+    </Switch>
+  </HashRouter>
+);
